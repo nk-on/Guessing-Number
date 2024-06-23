a418f453e8947447360c12f4b8ce1c3fcab3c4a2
nk-on <johndoe@example.com>
2024-06-23 23:12:40 +0400

    Defining variables

---
+++ dist/script.js
@@ -0,0 +1,8 @@
+"use strict";
+const startingNumContainer = document.querySelector('#starting');
+const endingNumContainer = document.querySelector('#end');
+const startingNumPlaceHolder = document.querySelector('[data-starting]');
+const endingNumPlaceHolder = document.querySelector('[data-ending]');
+const guessedNumContainer = document.querySelector('#guessing-num');
+const sumbitButton = document.querySelector('[data-submit]');
+const chancesPlaceHolder = document.querySelector('[data-chances]');
---
+++ src/script.ts
@@ -0,0 +1,7 @@
+const startingNumContainer = document.querySelector('#starting') as HTMLInputElement;
+const endingNumContainer = document.querySelector('#end') as HTMLInputElement;
+const startingNumPlaceHolder = document.querySelector('[data-starting]') as HTMLSpanElement;
+const endingNumPlaceHolder = document.querySelector('[data-ending]') as HTMLSpanElement;
+const guessedNumContainer = document.querySelector('#guessing-num') as HTMLInputElement;
+const sumbitButton = document.querySelector('[data-submit]') as HTMLInputElement;
+const chancesPlaceHolder = document.querySelector('[data-chances]') as HTMLSpanElement;
