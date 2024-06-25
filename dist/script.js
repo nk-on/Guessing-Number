747e482ba1de48d3fe0423eb2810e0c5fc27f434
nk-on <johndoe@example.com>
2024-06-25 20:55:50 +0400

    Feat:implementing feature which validates range and throws out error if startingNumber is more then ending number

--- dist/script.js
+++ dist/script.js
@@ -6,3 +6,14 @@ const endingNumPlaceHolder = document.querySelector('[data-ending]');
 const guessedNumContainer = document.querySelector('#guessing-num');
 const sumbitButton = document.querySelector('[data-submit]');
 const chancesPlaceHolder = document.querySelector('[data-chances]');
+function validateRange(e) {
+    e.preventDefault();
+    const startingNum = Number(startingNumContainer.value);
+    const endingNum = Number(endingNumContainer.value);
+    if (startingNum >= endingNum) {
+        alert('Starting Number should be less then ending number');
+        startingNumContainer.value = '';
+        endingNumContainer.value = '';
+    }
+}
+sumbitButton.addEventListener('click', validateRange);
--- src/script.ts
+++ src/script.ts
@@ -3,5 +3,16 @@ const endingNumContainer = document.querySelector('#end') as HTMLInputElement;
 const startingNumPlaceHolder = document.querySelector('[data-starting]') as HTMLSpanElement;
 const endingNumPlaceHolder = document.querySelector('[data-ending]') as HTMLSpanElement;
 const guessedNumContainer = document.querySelector('#guessing-num') as HTMLInputElement;
-const sumbitButton = document.querySelector('[data-submit]') as HTMLInputElement;
+const sumbitButton = document.querySelector('[data-submit]') as HTMLButtonElement;
 const chancesPlaceHolder = document.querySelector('[data-chances]') as HTMLSpanElement;
+function validateRange(e: Event): void {
+    e.preventDefault();
+    const startingNum: number = Number(startingNumContainer.value);
+    const endingNum: number = Number(endingNumContainer.value);
+    if (startingNum >= endingNum) {
+        alert('Starting Number should be less then ending number');
+        startingNumContainer.value = '';
+        endingNumContainer.value = '';
+    }
+}
+sumbitButton.addEventListener('click', validateRange)
