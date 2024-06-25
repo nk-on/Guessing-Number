158c5bbe5cad49ad9ab4984233382ac29bcb65b1
nk-on <johndoe@example.com>
2024-06-25 22:21:47 +0400

    Feat:implementing system which validates if user guessed number and if it is higher or low to targer num

--- dist/script.js
+++ dist/script.js
@@ -6,16 +6,22 @@ const endingNumPlaceHolder = document.querySelector('[data-ending]');
 const guessedNumContainer = document.querySelector('#guessing-num');
 const sumbitButton = document.querySelector('[data-submit]');
 const chancesPlaceHolder = document.querySelector('[data-chances]');
-const highLowPlaceholder = document.querySelector('[high-low]');
+const highLowPlaceholder = document.querySelector('[data-high-low]');
 function guessNumber(startingNum, endingNum) {
     let chances = 10;
     const targetNumber = Math.floor(Math.random() * (endingNum - startingNum + 1) + startingNum);
-    console.log(startingNum, endingNum);
     return () => {
-        if (targetNumber !== chances) {
+        console.log(targetNumber);
+        const guessedNum = Number(guessedNumContainer.value);
+        if (targetNumber !== guessedNum) {
             chances--;
             chancesPlaceHolder.textContent = String(chances);
         }
+        if (targetNumber === guessedNum) {
+            alert('You won !');
+        }
+        ;
+        guessedNum > targetNumber ? highLowPlaceholder.textContent = 'Too high' : highLowPlaceholder.textContent = 'Too low';
     };
 }
 function validateRange(e) {
--- src/script.ts
+++ src/script.ts
@@ -5,16 +5,21 @@ const endingNumPlaceHolder = document.querySelector('[data-ending]') as HTMLSpan
 const guessedNumContainer = document.querySelector('#guessing-num') as HTMLInputElement;
 const sumbitButton = document.querySelector('[data-submit]') as HTMLButtonElement;
 const chancesPlaceHolder = document.querySelector('[data-chances]') as HTMLSpanElement;
-const highLowPlaceholder = document.querySelector('[high-low]') as HTMLSpanElement;
-function guessNumber(startingNum: number, endingNum: number):  ()=> void {
-    let chances:number = 10;
-    const targetNumber:number = Math.floor(Math.random() * (endingNum - startingNum + 1) + startingNum);
-    console.log(startingNum,endingNum)
+const highLowPlaceholder = document.querySelector('[data-high-low]') as HTMLSpanElement;
+function guessNumber(startingNum: number, endingNum: number): () => void {
+    let chances: number = 10;
+    const targetNumber: number = Math.floor(Math.random() * (endingNum - startingNum + 1) + startingNum);
     return () => {
-        if(targetNumber !== chances){
+        console.log(targetNumber)
+        const guessedNum: number = Number(guessedNumContainer.value);
+        if (targetNumber !== guessedNum) {
             chances--;
             chancesPlaceHolder.textContent = String(chances);
         }
+        if(targetNumber === guessedNum){
+            alert('You won !');
+        };
+        guessedNum > targetNumber ? highLowPlaceholder.textContent = 'Too high' : highLowPlaceholder.textContent = 'Too low';
     }
 }
 function validateRange(e: Event): void {
@@ -26,7 +31,7 @@ function validateRange(e: Event): void {
         startingNumContainer.value = '';
         endingNumContainer.value = '';
     }
-    const guessNumFun = guessNumber(startingNum,endingNum);
+    const guessNumFun = guessNumber(startingNum, endingNum);
     guessNumFun();
 }
 sumbitButton.addEventListener('click', validateRange)
